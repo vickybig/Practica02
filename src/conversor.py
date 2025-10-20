d4df351645082685a9347e815ba547c39a8adc92
“Victor <vaimacana@est.ups.edu.ec>
2025-10-20 18:39:43 -0500

    Initial commit: Conversor básico USD a EUR/COP/GBP

---
+++ src/conversor.py
@@ -0,0 +1,31 @@
+import json
+from datetime import datetime
+
+def cargar_tasas(ruta):
+    """ Lee un archivo json y retorna un objeto"""
+    with open(ruta, "r") as archivo:
+        return json.load(archivo)
+    
+def convertir(precio_usd, moneda_destino, tasas):
+    """ Convierte el valor a otra moneda"""
+    #Obtiene la tasa de cambio de dolares a la moneda destino 
+    tasa = tasas["USD"].get(moneda_destino)
+    if not tasa:
+        #si la moneda de destino no existe , lanza una excepcion
+        raise ValueError("Moneda no soportada")
+    return precio_usd * tasa
+
+def registrar_transaccion(producto, precio_convertido, moneda, ruta_log):
+    """Escribe una nueva linea en el archivo de registro"""
+    with open(ruta_log, "a") as archivo:
+        #Obtener l fecha actual on formato Año-Mes-Dia / Horas-min-seg
+        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        #Escribir una linea nueva en el archivo de registro
+        archivo.write(f"{fecha} | {producto}: {precio_convertido:.2f} {moneda} \n")
+                                    
+# Ejemplo de uso
+if __name__ == "__main__":
+    tasas = cargar_tasas("../data/tasas.json")
+    precio_usd = 100.00
+    precio_eur = convertir(precio_usd, "EUR", tasas)
+    registrar_transaccion("Laptop", precio_eur, "EUR", "../logs/historial.txt")
